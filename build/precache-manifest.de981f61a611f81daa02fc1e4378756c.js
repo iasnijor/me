2fdef8eff51f36672d9d808edb26720f3e8e33df
Rojen <rojen.shahi3@gmail.com>
2020-05-19 14:54:09 -0400

    update4

--- build/precache-manifest.de981f61a611f81daa02fc1e4378756c.js
+++ build/precache-manifest.de981f61a611f81daa02fc1e4378756c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "eb3c850706642b69410c",
+    "revision": "94fa99ada107841ed354",
     "url": "/me/static/css/main.0ac0e183.chunk.css"
   },
   {
-    "revision": "eb3c850706642b69410c",
-    "url": "/me/static/js/main.eb3c8507.chunk.js"
+    "revision": "94fa99ada107841ed354",
+    "url": "/me/static/js/main.94fa99ad.chunk.js"
   },
   {
     "revision": "1caf0789ee8fdb1f0b3c",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/me/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "8d50c43d9c01cd8ec2f6606ac3757ae2",
+    "revision": "199d0cb563b269e4cbc36ab0ba69bcd8",
     "url": "/me/index.html"
   }
 ];
