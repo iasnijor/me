fc108bda174d857a455b865ccac002c9c3c13396
Rojen <rojen.shahi3@gmmail.com>
2020-07-18 00:02:31 -0400

    update badges2.2

---
+++ build/precache-manifest.baff3767ffb96bc6d45fb8315ddb9ae7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "936a1659b50d8ece7ae4",
+    "url": "/me/static/css/main.4b295461.chunk.css"
+  },
+  {
+    "revision": "936a1659b50d8ece7ae4",
+    "url": "/me/static/js/main.936a1659.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6da0461b12f2b2217a817de9d6496caf",
+    "url": "/me/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.667d068bf1926d033c4c2559fa787b40.js"
+  "/me/precache-manifest.baff3767ffb96bc6d45fb8315ddb9ae7.js"
 );
 
 workbox.clientsClaim();
